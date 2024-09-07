9b2ccf3457db45c77398da0eeb968d9e77dcd316
Shiva Yadav <shivayadavsy1256@gmail.com>
2024-09-07 15:15:54 +0530

    fix solution power of 2

--- NumberSystem/powerOfTwo.java
+++ NumberSystem/powerOfTwo.java
@@ -2,7 +2,7 @@
 public class powerOfTwo {
 
     static boolean isPowerOfTwo(int num) {
-        if (num < 0) {
+        if (num <= 0) {
             return false;
         }
         while (num != 0) {
